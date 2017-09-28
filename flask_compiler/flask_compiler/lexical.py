eae63c44aaebb5f51ee36619a3c5738bf17134b2
Fabio Takaki <fabio@takaki.me>
2017-09-28 13:57:32 -0300

    Size explode fixed

--- flask_compiler/flask_compiler/lexical.py
+++ flask_compiler/flask_compiler/lexical.py
@@ -4,46 +4,86 @@ class Lexical:
     self.code = code
 
   def run(self):
+    reserved = {
+        'program':'PALAVRA_RESERVADA_PROGRAM',
+        'procedure':'PALAVRA_RESERVADA_PROCEDURE',
+        'var':'PALAVRA_RESERVADA_VAR',
+        'begin':'PALAVRA_RESERVADA_BEGIN',
+        'end':'PALAVRA_RESERVADA_END',
+        'if':'PALAVRA_RESERVADA_IF',
+        'then':'PALAVRA_RESERVADA_THEN',
+        'else':'PALAVRA_RESERVADA_ELSE',
+        'while':'PALAVRA_RESERVADA_WHILE',
+        'do':'PALAVRA_RESERVADA_DO',
+    }
     # List of token names.   This is always required
-    tokens = (
+    tokens = [
       'OPSOMA',
       'OPSUB',
       'OPMUL',
       'OPDIV',
       'NUMERO',
       'IDENTIFICADOR',
-      'PALAVRA_RESERVADA',
-      'SIMBOLOS_ESPECIAIS',
-      'COMENTARIOS',
+      'SIMBOLOS_ESPECIAIS_MAIOR',
+      'SIMBOLOS_ESPECIAIS_MENOR',
+      'SIMBOLOS_ESPECIAIS_MENOR_IGUAL',
+      'SIMBOLOS_ESPECIAIS_MAIOR_IGUAL',
+      'SIMBOLOS_ESPECIAIS_ATRIBUICAO',
+      'SIMBOLOS_ESPECIAIS_DELIMITADOR',
+      'SIMBOLOS_ESPECIAIS_VIRGULA',
+      'SIMBOLOS_ESPECIAIS_DOIS_PONTOS',
+      'SIMBOLOS_ESPECIAIS_PONTO_FINAL',
+      'COMENTARIOS_UMA_LINHA',
+      'COMENTARIOS_MULTI_LINHA',
       'AP',
       'FP',
-    )
+    ] + list(reserved.values())
 
     # Regular expression rules for simple tokens
-    t_IDENTIFICADOR = r'[a-zA-Z]([a-zA-Z]|[0-9]|_)*'
-    t_PALAVRA_RESERVADA  = r'(program|procedure|var|begin|end|if|then|else|while|do)'
-    t_SIMBOLOS_ESPECIAIS = r'(>|<|<=|>=|:=|;|,|:|\.)'
+    def t_IDENTIFICADOR(t):
+      r'[a-zA-Z]([a-zA-Z]|[0-9]|_)*'
+      if len(t.value) > 15:
+        response.append(dict([('error', True), ('string', t.value[0]), ('line', t.lineno), ('start', t.lexpos), ('message', "Size string exceding 15 digits '%s'" % t.value)]))
+        t.lexer.skip(1)
+      else:
+        t.type = reserved.get(t.value,'IDENTIFICADOR')    # Check for reserved words
+        return t
+
+    t_SIMBOLOS_ESPECIAIS_MAIOR = r'>'
+    t_SIMBOLOS_ESPECIAIS_MENOR = r'<'
+    t_SIMBOLOS_ESPECIAIS_MENOR_IGUAL = r'<='
+    t_SIMBOLOS_ESPECIAIS_MAIOR_IGUAL = r'>='
+    t_SIMBOLOS_ESPECIAIS_ATRIBUICAO = r':='
+    t_SIMBOLOS_ESPECIAIS_DELIMITADOR = r';'
+    t_SIMBOLOS_ESPECIAIS_VIRGULA = r','
+    t_SIMBOLOS_ESPECIAIS_DOIS_PONTOS = r':'
+    t_SIMBOLOS_ESPECIAIS_PONTO_FINAL = r'\.'
     t_OPSOMA = r'\+'
     t_OPSUB  = r'-'
     t_OPMUL  = r'\*'
-    t_OPDIV  = r'^/$'
+    t_OPDIV  = r'/'
     t_AP     = r'\('
     t_FP     = r'\)'
 
     # comments
-    def t_COMENTARIOS(t):
-      r'{(.|\n)*}|(//.*)'
-      if(t.value[0] == '{'):
-        t.value = '{}'
-      else:
-        t.value = '//'
-      return t
+    def t_COMENTARIOS_UMA_LINHA(t):
+      r'(//.*)'
+      t.value = '//'
+      return t 
 
+    def t_COMENTARIOS_MULTI_LINHA(t):
+      r'{(.|\n)*}'
+      t.value = '{}'
+      return t
 
     # A regular expression rule with some action code
     def t_NUMERO(t):
       r'\d+\.?\d*'
-      return t
+      if len(t.value) > 5:
+        response.append(dict([('error', True), ('string', t.value[0]), ('line', t.lineno), ('start', t.lexpos), ('message', "Size number exceding 5 digits '%s'" % t.value)]))
+        t.lexer.skip(1)
+      else:  
+        return t
 
     # Define a rule so we can track line numbers
     def t_newline(t):
